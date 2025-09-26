0254afcf4e7d912e31eb065fadd3f723c5364334
genario100 <76125969+genario100@users.noreply.github.com>
2025-09-26 18:03:28 -0300

    Adicionar arquivos de projeto.

---
+++ App.xaml.cs
@@ -0,0 +1,21 @@
+﻿namespace MauiAppMinhasCompras
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new Views.ListaProduto());
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            var window = base.CreateWindow(activationState);
+            window.Title = "Minhas Compras";
+            window.Width = 450;
+            window.Height = 900;
+            return window;
+        }
+    }
+}
